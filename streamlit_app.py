30580f572d6d918512a59379fb4b43241cca2193
Adcreative Workshop <139262376+AdcreativeDev@users.noreply.github.com>
2023-07-13 08:16:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,28 +16,7 @@ icon_size = 20
 
 
 
-st_button('medium', 'https://import-2-vector.streamlit.app', 'vectorization to embedding', icon_size)
-st_button('medium', 'https://hug-bot.streamlit.app/', 'Hug Chat', icon_size)
-st_button('medium', 'https://recommend-cs.streamlit.app/', 'recommend products', icon_size)
-st_button('medium', 'https://prompt-runner.streamlit.app/', 'Prompt Runner', icon_size)
-st_button('medium', 'https://pdf-automation.streamlit.app/', 'Ask Resume PDF file II', icon_size)
-st_button('medium', 'https://askpdf-by-david.streamlit.app/', 'Ask Resume PDF file I', icon_size)
-st_button('medium', 'https://csv-sqldb.streamlit.app/', 'Ask any CSV file', icon_size)
-st_button('medium', 'https://smart-csv.streamlit.app/', 'Ask  CSV by ai', icon_size)
-
-st_button('medium', 'https://ask-studyplan.streamlit.app/', 'Summarize your Study Plan', icon_size)
-st_button('medium', 'https://ask-orders-csv.streamlit.app/', 'Summarize your Customers Orders', icon_size)
-st_button('medium', 'https://xls-loader.streamlit.app/', 'Visualize your Excel Sheet', icon_size)
-
-
-
-
-
-
-# Generative Creation 
-st_button('medium', 'https://storymaker-by-david.streamlit.app/', 'Create your Story', icon_size)
-st_button('medium', 'https://questions-maker-by-david.streamlit.app/', 'Create your Article & Questions-Answer', icon_size)
-st_button('medium', 'https://story-ppt-david.streamlit.app/', 'See Comic Book created by MidJourney ', icon_size)
-# Search & Analsis
-st_button('medium', 'https://search-by-david.streamlit.app/', 'Google Now', icon_size)
-st_button('medium', 'https://stockwatch-by-david.streamlit.app/', 'Stock Watch', icon_size)
+st_button('medium', 'https://import-2-vector.streamlit.app', 'NewsAPI extraction and query', icon_size)
+ 
+ 
+ 
